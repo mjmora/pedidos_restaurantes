e4f716efb44f9140f05a08ff490222771c847f62
diegotony <tucotony1396@gmail.com>
2018-01-28 18:42:58 -0500

    mis cambios

--- restaurantp/restaurantp/settings.py
+++ restaurantp/restaurantp/settings.py
@@ -76,11 +76,15 @@ WSGI_APPLICATION = 'restaurantp.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+	    'default': {
+	        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+	        'NAME': 'restaurant',
+	        'USER': 'redes',
+	        'PASSWORD': '12345',
+	        'HOST': '127.0.0.1',
+	        'PORT': '5432',
+	    }
+	}
 
 
 # Internationalization
