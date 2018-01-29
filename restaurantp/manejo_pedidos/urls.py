fa0069adda0d1c8f31d01cbffef63de9e0140b95
María José Mora <mjmora@github.com>
2018-01-29 18:44:04 -0500

    Vista registro, menu

--- restaurantp/manejo_pedidos/urls.py
+++ restaurantp/manejo_pedidos/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'scanqr$', views.escaneo, name='escaneo'),
     url(r'cliente$', views.reg_cliente , name='cliente'),
-
+    url(r'menu$', views.menu, name='menu'),
 
 
 
--- restaurantp/manejo_pedidos/views.py
+++ restaurantp/manejo_pedidos/views.py
@@ -9,14 +9,13 @@ from django.template import RequestContext
 from django.http import Http404
 from django.views.decorators.csrf import csrf_exempt
 from django.core import serializers
-<<<<<<< HEAD
+
 #from .forms import PostForm
 
 from manejo_pedidos.models import *
-=======
 from django.utils import timezone
 from django.shortcuts import redirect
->>>>>>> a7a70c0bc58437b9bfa2bf9fcf026323cb02236e
+
 
 def index(request):
 	#nuevo = Mesa(numero=1,capacidad=4)
@@ -36,17 +35,7 @@ def menu(request):
 		context_instance = RequestContext(request))
 
 
-<<<<<<< HEAD
-#def post_new(request):
-	#from = PostForm()
-#	if request.methos == "POST":
-#		form=PostForm(request.POST)
-#		if form.is_valid():
-#			post = form.save(commit=False)
-#	else:
-#		form=PostForm()
-#	return render(request,'',{'form':form})
-=======
+
 
 def reg_cliente(request):
 	if request.method == "POST":
@@ -62,17 +51,3 @@ def reg_cliente(request):
 	return render(request, 'cliente.html', {'form': form})
 
 
-
-
->>>>>>> a7a70c0bc58437b9bfa2bf9fcf026323cb02236e
-
-# def pedido_cliente(request):
-#     """
-#     """
-#     req = {}#crea un diccionario vacio
-#     if request.is_ajax() == True:
-#         nombre = request.POST['nombre']#hace referencia al valor enviado enel tmplate paralelo_buscador
-#
-#         req['mensaje'] = 'Correcto .... cargando datos '
-#         return render(request, 'cliente.html',req,
-# 		context_instance = RequestContext(request))
